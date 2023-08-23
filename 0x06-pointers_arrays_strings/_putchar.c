fab64d1bd1ea1320cead66c920e9575e97e4b966
AboudM1 <abdomohammed121@gmail.com>
2023-08-23 12:16:30 -0700

    0x06

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/** 
+  *+putchar writes the character c to stdout
+  *@c: the character to print
+  *
+  * Return on succes 1
+  * On error -1 is returned
+  */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
